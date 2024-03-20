15f9ea803fdd1476930bf005234a0150cfa9808f
Lorenzo Lamberti <lorenzo.lamberti@unibo.it>
2024-03-20 10:26:02 +0100

    sdk 3.9.1 on the right commits

---
+++ source_3.9.1.sh
@@ -0,0 +1,9 @@
+# 3.9.1
+export GAP_RISCV_GCC_TOOLCHAIN="/home/lamberti/gap/3.9.1/gap_riscv_toolchain"
+export GAPY_OPENOCD_CABLE=interface/ftdi/olimex-arm-usb-ocd-h.cfg
+source $HOME/gap/3.9.1/gap_sdk/configs/ai_deck.sh
+conda activate gap_sdk
+
+# PULP_SDK
+# export PULP_RISCV_GCC_TOOLCHAIN="/home/lamberti/v1.0.16q-pulp-riscv-gcc-ubun
+
